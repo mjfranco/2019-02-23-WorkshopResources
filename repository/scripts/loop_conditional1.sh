b9feb0af2c340a97d209c13eac801c59c5caa454
mjfranco <mjfranco@email.arizona.edu>
2019-02-23 16:32:48 -0700

    added repository

---
+++ repository/scripts/loop_conditional1.sh
@@ -0,0 +1,12 @@
+# $1 is field to cut
+# $2 specifies high or low value to output 
+for gapminderfile in [U-Z]*.txt
+do
+    echo $gapminderfile
+    if [ "$2" = "high" ] # test whether 2nd argument is "high"
+    then
+            cut -f "$1" $gapminderfile | sort -n | tail -1
+    else
+            cut -f "$1" $gapminderfile | sort -n | head -1
+    fi # Dont forget to end if conditional
+done
